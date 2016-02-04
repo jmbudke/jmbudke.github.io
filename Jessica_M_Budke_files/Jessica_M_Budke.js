db2be1286d18cf1ab9f0320941b4d95b056065bc
Jessica Budke <jessica.m.budke@gmail.com>
2016-02-04 15:18:09 -0800

    Fixing email

--- Jessica_M_Budke_files/Jessica_M_Budke.js
+++ Jessica_M_Budke_files/Jessica_M_Budke.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20160120
+// Created by iWeb 3.0.4 local-build-20160204
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke(),reflection_0:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
--- Teaching_files/Teaching.js
+++ Teaching_files/Teaching.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20160120
+// Created by iWeb 3.0.4 local-build-20160204
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.38,offset:2.00})});registry.applyEffects();}
