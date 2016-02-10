7dd6e8671dbf7c338358af6c8ed7cefe9e059daa
Jessica Budke <jessica.m.budke@gmail.com>
2016-02-09 21:21:09 -0800

    Deleting the M

--- Jessica_Budke_files/Jessica_Budke.js
+++ Jessica_Budke_files/Jessica_Budke.js
@@ -1,11 +1,11 @@
-// Created by iWeb 3.0.4 local-build-20160204
+// Created by iWeb 3.0.4 local-build-20160209
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke(),reflection_0:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
-{loadMozillaCSS('Jessica_M_Budke_files/Jessica_M_BudkeMoz.css')
+{loadMozillaCSS('Jessica_Budke_files/Jessica_BudkeMoz.css')
 adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
