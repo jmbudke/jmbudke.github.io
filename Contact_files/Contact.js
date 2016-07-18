f2a6952c24722bd0cecc5559f996dd07dd2de7c0
Jessica Budke <jessica.m.budke@gmail.com>
2016-07-17 21:01:07 -0400

    UTK21

--- Contact_files/Contact.js
+++ Contact_files/Contact.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20160621
+// Created by iWeb 3.0.4 local-build-20160717
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
