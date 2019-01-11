5d1295240103d9e6226fa8353638a4eab0611fa0
Jessica Budke <jessica.m.budke@gmail.com>
2019-01-11 13:02:20 -0500

    2019 Jan Update

--- Outreach_files/Outreach.js
+++ Outreach_files/Outreach.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20181028
+// Created by iWeb 3.0.4 local-build-20190111
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWEmptyStroke(),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
